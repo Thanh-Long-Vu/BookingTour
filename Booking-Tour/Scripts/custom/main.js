a29de9fb0a9034ae5f727188df612c13374e6935
hapo-chinhnc <chinh.nc@haposoft.com>
2020-12-15 22:11:50 +0700

    update upload img user

--- Booking-Tour/Scripts/custom/main.js
+++ Booking-Tour/Scripts/custom/main.js
@@ -334,4 +334,20 @@ $(document).ready(function () {
 		$('p[id="payment"]').html(payment + '$');
 	});
 
+	function readURL(input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+
+			reader.onload = function (e) {
+				$('#blah').attr('src', e.target.result);
+			}
+
+			reader.readAsDataURL(input.files[0]); // convert to base64 string
+		}
+	}
+
+	$("#imgInp").change(function () {
+		readURL(this);
+	});
+
 });
